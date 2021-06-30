465f9e8a99b7f760eecaa73a9187638f9dde7f54
blackpanther8132 <blackpanther8132@gmail.com>
2021-06-30 16:42:47 +0530

    updating build

--- build/transaction/bank/sendCoin.js
+++ build/transaction/bank/sendCoin.js
@@ -72,13 +72,13 @@ class bank extends persistenceJS_1.Persistence {
             let path = this.path;
             let options = {
                 method: "POST",
-                url: path + "/bank/accounts/" + from_address + "/transfers",
+                url: path + "/bank/accounts/" + to_address + "/transfers",
                 headers: {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
                     base_req: {
-                        from: to_address,
+                        from: from_address,
                         chain_id: chain_id,
                         memo: memo,
                         fees: [{ amount: String(feesAmount), denom: feesToken }],
